4e32fa34c7d0819d967da78c7fcbc5587d11b98a
Jesus Roldan <xebuz18@gmail.com>
2016-01-31 23:08:16 -0300

    Fix Tests

--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,6 @@
 import unittest
-from flipexception import FlipValueError
+from flipexception import FlipException
+
 
 class FlipAllTheThings(unittest.TestCase):
     def setUp(self):
@@ -11,12 +12,12 @@ class FlipAllTheThings(unittest.TestCase):
         """
 
         with self.assertRaises(Exception):
-            raise FlipValueError()
+            raise FlipException()
 
     def test_exception_flip(self):
         """
         Testing Eexception Base
         """
 
-        with self.assertRaises(FlipValueError):
-            raise FlipValueError()
+        with self.assertRaises(FlipException):
+            raise FlipException()
