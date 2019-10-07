21563a2101cf601805197a85a82b043ede656388
Jesús Roldán <jesus.roldan@gmail.com>
2019-10-07 16:57:36 +0200

    Not needed

--- flipexception/flipexception.py
+++ flipexception/flipexception.py
@@ -2,20 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import upsidedown
-import sys
-
-try:
-    reload  # Python 2.7
-except NameError:
-    try:
-        from importlib import reload  # Python 3.4+
-    except ImportError:
-        from imp import reload  # Python 3.0 - 3.3
-
-sys.setdefaultencoding("utf-8")
 
 __version__ = 0.5
-
 
 class FlipException(Exception):
 
